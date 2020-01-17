2d5f0d10b8bbfacbc6642d2259473342d82205b0
Benjamin Hansen <benjamin.hansen44@gmail.com>
2020-01-17 10:04:19 +0100

    opdatde changeTheme.js

--- app/src/javascript/changeTheme.js
+++ app/src/javascript/changeTheme.js
@@ -1,8 +1,6 @@
 var currentTheme = localStorage.getItem("currentTheme");
 document.documentElement.setAttribute("data-theme", currentTheme);
 const changeTheme = document.querySelector(".changeTheme");
-const bottomMenu = document.querySelector(".footer__nav");
-console.log(bottomMenu.style.background);
 
 changeTheme.addEventListener("click", () => {
   if (document.documentElement.dataset.theme == "light") {
